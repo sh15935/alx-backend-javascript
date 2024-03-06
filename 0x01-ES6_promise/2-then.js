a5f884852341c19fd032d33a07153a501b7cda89
sh15935 <safazsahih@gmail.com>
2024-03-06 23:20:39 +0000

    tasks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,12 @@
+// Promise is handle or not
 function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => ({
-                status: 200,
-                body: 'success',
-        }))
-        .catch(() => Error())
-        .finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
