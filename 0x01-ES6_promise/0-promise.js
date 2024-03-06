e1897469cce2dc775731a0e06b2542b10880da80
sh15935 <safazsahih@gmail.com>
2024-03-06 23:24:07 +0000

    tasks

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,14 @@
+// Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-	    if (true) {
-		    resolve();
-            } else {
-		    reject();
-	    }
-    });
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+      /* eslint-enable */
+  });
 }
 
 export default getResponseFromAPI;
