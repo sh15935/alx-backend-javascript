839eea6a667d1afab79ff26dac52033ee3ff6998
sh15935 <safazsahih@gmail.com>
2024-03-07 17:09:31 +0000

    tasks

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -5,6 +5,6 @@ export default function createReportObject(employeesList) {
     },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
-    }
+    },
   };
 }
